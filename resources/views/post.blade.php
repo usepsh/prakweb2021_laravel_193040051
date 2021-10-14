283658f24bad38d9fd5908ffdb160dc9c77322ff
usepsh <58686569+usepsh@users.noreply.github.com>
2021-10-14 14:48:13 +0700

    mempelajari model, collection & controller

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,5 +6,5 @@
     <h5>{{ $post["author"] }}</h5>
     <p>{{ $post["body"] }}</p>
 </article>
-<a href="/blog">Back to Posts</a>
+<a href="/posts">Back to Posts</a>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,54 +33,7 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Sasuke Shippuden",
-            "slug" => "sasuke-shippuden",
-            "author" => "Usep Yys",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit rerum ab quas, maiores id a est velit, ut explicabo magni fuga, placeat quaerat culpa. Esse, quisquam vitae. Aspernatur, laudantium illo. Dignissimos saepe officiis nostrum quidem perspiciatis quisquam ab deserunt sapiente, fugit dolor cum architecto corporis repudiandae quia eius adipisci, quos maiores aliquid deleniti minus cumque, perferendis ea fugiat aliquam. Recusandae dolorem omnis nihil eos ducimus obcaecati, incidunt quaerat neque facere impedit fugit perferendis, quis accusamus delectus quisquam laudantium culpa ea?"
-        ],
-        [
-            "title" => "Sakura Shippuden",
-            "slug" => "sakura-shippuden",
-            "author" => "Jennie Yys",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit rerum ab quas, maiores id a est velit, ut explicabo magni fuga, placeat quaerat culpa. Esse, quisquam vitae. Aspernatur, laudantium illo. Dignissimos saepe officiis nostrum quidem perspiciatis quisquam ab deserunt sapiente, fugit dolor cum architecto corporis repudiandae quia eius adipisci, quos maiores aliquid deleniti minus cumque, perferendis ea fugiat aliquam. Recusandae dolorem omnis nihil eos ducimus obcaecati, incidunt quaerat neque facere impedit fugit perferendis, quis accusamus delectus quisquam laudantium culpa ea?"
-        ],
-    ];
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
+Route::get('/posts', [PostController::class, 'index']);
 
 // Halaman single post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Sasuke Shippuden",
-            "slug" => "sasuke-shippuden",
-            "author" => "Usep Yys",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit rerum ab quas, maiores id a est velit, ut explicabo magni fuga, placeat quaerat culpa. Esse, quisquam vitae. Aspernatur, laudantium illo. Dignissimos saepe officiis nostrum quidem perspiciatis quisquam ab deserunt sapiente, fugit dolor cum architecto corporis repudiandae quia eius adipisci, quos maiores aliquid deleniti minus cumque, perferendis ea fugiat aliquam. Recusandae dolorem omnis nihil eos ducimus obcaecati, incidunt quaerat neque facere impedit fugit perferendis, quis accusamus delectus quisquam laudantium culpa ea?"
-        ],
-        [
-            "title" => "Sakura Shippuden",
-            "slug" => "sakura-shippuden",
-            "author" => "Jennie Yys",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit rerum ab quas, maiores id a est velit, ut explicabo magni fuga, placeat quaerat culpa. Esse, quisquam vitae. Aspernatur, laudantium illo. Dignissimos saepe officiis nostrum quidem perspiciatis quisquam ab deserunt sapiente, fugit dolor cum architecto corporis repudiandae quia eius adipisci, quos maiores aliquid deleniti minus cumque, perferendis ea fugiat aliquam. Recusandae dolorem omnis nihil eos ducimus obcaecati, incidunt quaerat neque facere impedit fugit perferendis, quis accusamus delectus quisquam laudantium culpa ea?"
-        ],
-    ];
-
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/posts/{slug}', [PostController::class, 'show']);
